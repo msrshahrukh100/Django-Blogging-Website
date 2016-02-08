51d8909643e4b310fc0b99897121a8bb5093cc26
shahrukh <msr.concordfly@gmail.com>
2016-02-08 21:14:59 +0530

    Template Inheritence

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Post
+from django.contrib import messages
 
+from .forms import PostForm
 # Create your views here.
 
 
 def post_create(request):
-	return HttpResponse("<h1>Created</h1>") 
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request,"Successfully Created!")
+		return HttpResponseRedirect(instance.get_absolute_url())
+	
+
+	context = {
+	"form":form,
+	}
+	return render(request,'post_form.html',context) 
 
 def post_detail(request,id):
 	instance = get_object_or_404(Post,id=id)
@@ -23,13 +36,29 @@ def post_list(request):
 	"object_list":queryset,
 	"title":"List"
 	}
-	return render(request,'index.html',context)
+	return render(request,'base.html',context)
 	# return HttpResponse("<h1>List</h1>") 
 
 
-def post_update(request):
-	return HttpResponse("<h1>update</h1>") 
+def post_update(request,id=None): 
+	instance = get_object_or_404(Post,id=id)
+	form = PostForm(request.POST or None,instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request,"Successfully Updated!")
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context = {
+	"form":form,
+	}
+	return render(request,'post_form.html',context) 
 
 
-def post_delete(request):
-	return HttpResponse("<h1>delete</h1>") 
+
+
+def post_delete(request,id=None):
+	instance = get_object_or_404(Post,id=id)
+	instance.delete()
+	messages.success(request,"Successfully Deleted!")
+	return redirect("/posts")
+	 
