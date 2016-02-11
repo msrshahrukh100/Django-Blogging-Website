928fd6fe3340125eada79fd98aaa1a2803c3ab91
shahrukh <msr.concordfly@gmail.com>
2016-02-11 22:35:27 +0530

    Image addeded and displayed

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,8 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-	title = models.CharField(max_length=10)
+	title = models.CharField(max_length=50)
+	image = models.FileField(blank=True,null=True)
 	content = models.TextField()
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -19,3 +20,6 @@ class Post(models.Model):
 	def get_absolute_url(self):
 		return "/posts/%s/" %(self.id)
 	
+
+	class Meta:
+		ordering=["-timestamp"]
--- posts/views.py
+++ posts/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from .models import Post
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .forms import PostForm
 # Create your views here.
 
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None,request.FILES or None )
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
 		messages.success(request,"Successfully Created!")
 		return HttpResponseRedirect(instance.get_absolute_url())
 	
-
 	context = {
 	"form":form,
 	}
@@ -31,18 +31,41 @@ def post_detail(request,id):
 	
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_total = Post.objects.all()#.order_by("-timestamp")
+	# contact_list = Contacts.objects.all()
+	paginator = Paginator(queryset_total, 2) # Show 25 contacts per page
+
+	page = request.GET.get('page')
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
+	# return render(request, 'list.html', {'contacts': contacts})
+
 	context = {
-	"object_list":queryset,
-	"title":"List"
+		"object_list":queryset,
+		"title":"List"
 	}
 	return render(request,'base.html',context)
 	# return HttpResponse("<h1>List</h1>") 
 
+#
+
+    
+
+#
+
+
+
 
 def post_update(request,id=None): 
 	instance = get_object_or_404(Post,id=id)
-	form = PostForm(request.POST or None,instance=instance)
+	form = PostForm(request.POST or None,request.FILES or None,instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
